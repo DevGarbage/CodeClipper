3ff9d445218d392d1fbbdfad709b615dc9429c68
ravinsinghd <ravinsinghd@outlook.com>
2015-08-30 19:08:16 +0530

    content script modified for test the web page type (common, frontend playsites)

--- index.js
+++ index.js
@@ -1,28 +1,41 @@
+//SDK file import
 var buttons = require('sdk/ui/button/action');
 var tabs = require("sdk/tabs");
 var panels = require("sdk/panel");
 var self = require("sdk/self");
-
+var pageMod = require("sdk/page-mod");
+var jsonData=self.data.load('frontendPlaySites/sitesList.json');
+//Browser Button
 var button = buttons.ActionButton({
-  id: "CodeClipper",
-  label: "Code Clipper",
-  icon: {
-    "16": "./Icons/Code-16.png",
-    "32": "./Icons/Code-32.png",
-    "64": "./Icons/Code-64.png"
-  },
-  onClick: handleClick
+    id: "CodeClipper",
+    label: "Code Clipper",
+    icon: {
+        "16": "./Icons/Code-16.png",
+        "32": "./Icons/Code-32.png",
+        "64": "./Icons/Code-64.png"
+    },
+    onClick: handleClick
 });
-
+//Action Button panel
 var panel = panels.Panel({
-  contentURL: self.data.url("panel/panel.html"),
+    contentURL: self.data.url("panel/panel.html"),
 });
-function handleClick(state) {
-    panel.show({
-      position: button
+//Loading Content scripts
+pageMod.PageMod({
+  include: "*",
+  contentScriptFile:[self.data.url('scripts/contentScripts/jquery-2.1.4.min.js'),
+                       self.data.url("scripts/contentScripts/pageLoad.js")],
+                        attachTo: ["existing", "top"],
+                       onAttach: function(worker) {
+  
+  worker.port.on("FrontendSitesListReq", function(data) {
+      worker.port.emit("FrontendSitesListRes",jsonData);
     });
 }
- tabs.activeTab.attach({
-       contentScriptFile: [self.data.url('scripts/contentScripts/jquery-2.1.4.min.js'),
-                          self.data.url("scripts/contentScripts/pageLoad.js")]
+});
+//Action button click handler
+function handleClick(state) {
+    panel.show({
+        position: button
     });
+}
