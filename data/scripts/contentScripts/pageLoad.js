39d97674dd53be000038ec5c12bd4e223e20e1e9
ravinsinghd <ravinsinghd@outlook.com>
2015-09-01 01:08:25 +0530

    added clipper button for commom pages

--- data/scripts/contentScripts/pageLoad.js
+++ data/scripts/contentScripts/pageLoad.js
@@ -22,8 +22,17 @@ self.port.on("FrontendSitesListRes", function (jsonData) {
     }
 });
 function CommonWebPage() {
-    alert('common');
+    AddClipperInCode();
 }
 function FrontendPlayWebPage() {
     alert('frontend play site');
+}
+function AddClipperInCode() {
+    $('pre').each(function (index, ele) {
+        var btnstr = '<div class="btn-group"> <button type="button" class="btn btn-danger">Action</button> <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <span class="caret"></span> <span class="sr-only">Toggle Dropdown</span> </button> <ul class="dropdown-menu"> <li><a href="#">Action</a></li> <li><a href="#">Another action</a></li> <li><a href="#">Something else here</a></li> <li role="separator" class="divider"></li> <li><a href="#">Separated link</a></li> </ul> </div>';
+        var $btn = $.parseHTML(btnstr);
+        $($btn).css("float", "right");
+        $(ele).prepend($btn);
+
+    });
 }
